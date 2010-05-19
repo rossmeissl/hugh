b25bdc18dbe0cc348c7a007cdcec1ff29011aade
Andy Rossmeissl <andy@rossmeissl.net>
2010-05-18 22:07:40 -0400

    Give the track an ID

--- generators/hugh_assets/templates/hugh.js
+++ generators/hugh_assets/templates/hugh.js
@@ -2,7 +2,7 @@ var foo;
 var Hugh = Class.create({
     initialize: function(el) {
       this.el = $(el);
-      this.track = new Element('div', { 'class': 'hugh hugh-track' });
+      this.track = new Element('div', { 'class': 'hugh hugh-track', 'id': this.el.id + '_track' });
       this.handle = new Element('div', { 'class': 'hugh hugh-handle' });
       this.track.insert({ bottom: this.handle });
       this.el.insert({ after: this.track });
--- hugh.gemspec
+++ hugh.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Andy Rossmeissl"]
-  s.date = %q{2010-05-17}
+  s.date = %q{2010-05-18}
   s.description = %q{A slider for picking a color hue built with Scriptaculous and Prototype}
   s.email = %q{andy@rossmeissl.net}
   s.extra_rdoc_files = [
